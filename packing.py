ba456e4b321dc0dce11d17afc15a1f745c4c2cae
SnarkyGoblin123 <antojoel625@gmail.com>
2024-10-12 15:42:38 +0530

    Update

--- packing.py
+++ packing.py
@@ -1,12 +1,16 @@
 import numpy as np
 import goldcodegenerator as gcg
 import struct
+import copy
+import sys
+
+k = float(sys.argv[1])
 
 filepath = '/home/joel/gps-sdr-sim-master/gen_data_split/mydataa'
 data = np.fromfile(filepath,dtype = np.short)
 compdata = data[::2] + (1j)*data[1::2]
 
-sv = 23
+sv = 10
 prn = gcg.PRN(sv)
 prn = [-1 if x == 0 else x for x in prn]
 
@@ -41,29 +45,37 @@ v1 = np.array(v1).flatten()
 
 # Shift the complex data by the Doppler frequency
 f_d = doppler[sv]
-n = np.arange(len(compdata[:2000000]))
-compdata_shifted = np.multiply(compdata[:2000000],np.exp(-1j*2*np.pi*f_d*n/4000000))
+n = np.arange(len(compdata))
+compdata_shifted = np.multiply(compdata,np.exp(-1j*2*np.pi*f_d*n/4000000))
 
 # After performing the correlation in test.ipynb, we take the first maxima which comes out to be at 3200
-y = compdata_shifted[3200: 3200 + 640000]
+y = copy.deepcopy(compdata_shifted[120323923: 120323923 + 640000])
 x = v1
 
 # Least squares estimation for the eqn y = alp*x + noise
 alp_est = np.dot(x.T, y)/np.dot(x.T,x)
-alp_est = 0.0 * alp_est
+alp_est = k * alp_est
 
 # Subtract the estimated alp*x from y to remove satellite 23 component for atleast those 640000 samples
 yhat = y - alp_est*x
 
 
 # Perform inverse doppler shift to get the original signal
-yhat = np.multiply(yhat, np.exp(1j*2*np.pi*f_d*n[3200: 3200 + 640000]/4000000))
-print (np.array_equal(yhat, compdata[3200: 3200 + 640000]))
+yhat = np.multiply(yhat, np.exp(1j*2*np.pi*f_d*n[120323923: 120323923 + 640000]/4000000))
+# print (np.array_equal(yhat, compdata[3200: 3200 + 640000]))
 # Update the complex data with the new data
-compdata_upd = compdata
-compdata_upd[3200: 3200 + 640000] = yhat
+compdata_upd = copy.deepcopy(compdata)
 
-print (np.array_equal(compdata_upd, compdata))
+del compdata
+del n
+del compdata_shifted
+
+compdata_upd[120323923: 120323923 + 640000] = yhat
+
+
+# print (np.array_equal(compdata_upd, compdata))
+compdata_upd = np.round(compdata_upd)
+# print (np.array_equal(compdata_upd, compdata))
 # Save the updated data
 # Separate the real (I) and imaginary (Q) parts
 I_data = np.real(compdata_upd).astype(np.short)
@@ -72,6 +84,7 @@ Q_data = np.imag(compdata_upd).astype(np.short)
 
 
 # print (np.array_equal(I_data, data[::2]))
+# print (np.array_equal(Q_data, data[1::2]))
 # print(np.allclose(I_data, data[::2]))
 # print(np.allclose(Q_data, data[1::2]))
 
@@ -80,16 +93,23 @@ interleaved_data = np.empty((I_data.size + Q_data.size,), dtype=np.short)
 interleaved_data[::2] = I_data
 interleaved_data[1::2] = Q_data
 
-# print (np.allclose(interleaved_data[3200:6400+1280000], data[3200:6400+1280000]))
+# print (np.array_equal(interleaved_data, data))
+# for i in range(200):
+#   print (i)
+#   print (interleaved_data[i], data[i])
+
+      
 
 # Pack the interleaved data into binary format
 # packed_data = struct.pack(f'<{len(interleaved_data)}h', *interleaved_data)
 packed_data = interleaved_data.tobytes()
 
 # Write the packed data to a .dat file
-output_filepath = 'data/output_upd.dat'
+output_filepath = '/home/joel/gps-sdr-sim-master/gen_data_split/output_upd.dat'
 with open(output_filepath, 'wb') as f:
     f.write(packed_data)
+    b1 = bytearray(b'_')
+    f.write(b1)
 
 print(f"Packed IQ data written to {output_filepath}")
 
--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 # Set variables
 PYTHON_SCRIPT="packing.py"  # Replace with the name of your Python script
 DEST_DIR="/home/joel/gps-sdr-sim-master/gen_data_split"  # Replace with your destination folder path
-FINAL_DEST_DIR="/home/joel/work/data"  # Replace with final destination folder path
+FINAL_DEST_DIR="/home/joel/BTP/data"  # Replace with final destination folder path
 CONFIG_FILE="./test.conf"
 COMBINED_OUTPUT="combined_data.dat"
 
 source ../Documents/env_fold/python3.10/bin/activate
 
 # Define the range of 'k' values (e.g., from 0.01 to 0.05 with a step of 0.01)
-for k in $(seq 0.01 0.01 0.01); do
+for k in $(seq -2 1 3); do
     # Run the Python script with 'k' as an argument
     python3 $PYTHON_SCRIPT $k
 
@@ -27,15 +27,15 @@ for k in $(seq 0.01 0.01 0.01); do
     # Move the combined output to the final destination folder
     mv $COMBINED_OUTPUT $FINAL_DEST_DIR/combined_data.dat
     
-    cd /home/joel/work/data
-    sudo chmod 777 combined_data.dat
-    cd ../
+    cd /home/joel/BTP
+    # sudo chmod 777 combined_data.dat
+    # cd ../
 
     # Run the GNSS-SDR tool with the specified config file and save the log output
-    gnss-sdr --config_file=$CONFIG_FILE > log_1_alp.txt
+    gnss-sdr --config_file=$CONFIG_FILE > log_k_${k}.txt
 
     # Completion message
-    echo "Process completed for k=$k. Combined data file moved to $FINAL_DEST_DIR/combined_data_k_${k}.dat. Log saved as log_${kalp}_k_${k}.txt"
+    echo "Process completed for k=$k. Combined data file moved to $FINAL_DEST_DIR/combined_data_k_${k}.dat. Log saved as log_k_${k}.txt"
 
     # Optionally, you can clean up the individual output file to save space
     # rm $OUTPUT_FILE
